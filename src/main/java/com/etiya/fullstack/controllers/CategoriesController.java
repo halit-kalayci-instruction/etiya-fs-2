8e25459b8ba68d62dd904132c3b2681487cc9dce
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-09 09:58:28 +0300

    controllers

---
+++ src/main/java/com/etiya/fullstack/controllers/CategoriesController.java
@@ -0,0 +1,32 @@
+package com.etiya.fullstack.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/categories")
+public class CategoriesController {
+    @GetMapping
+    public void getAll(){
+
+    }
+
+    @GetMapping("{id}")
+    public void getById(@PathVariable int id){
+
+    }
+
+    @PostMapping
+    public void add(){
+
+    }
+
+    @PutMapping()
+    public void update(){
+
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id){
+
+    }
+}
