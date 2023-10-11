c9faf18d5b13c3b0ec366889b0f5f3c4f6c0a350
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-11 09:53:17 +0300

    jwt service

---
+++ src/main/java/com/etiya/fullstack/core/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.etiya.fullstack.core.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+            final String authHeader = request.getHeader("Authorization");
+            //Eğer auth headeri gönderilmemiş veya yanlış gönderilmiş ise
+            if(authHeader==null || !authHeader.startsWith("Bearer ")){
+                filterChain.doFilter(request,response);
+                return;
+            }
+
+            final String jwt = authHeader.substring(7);
+            final String username = jwtService.extractUsername(jwt);
+
+            // username null değilse ve böyle bir user varsa?
+            // token süresi geçmemiş ve benim ürettiğim bir tokensa
+
+    }
+}
