4db8b082c6df871ff0a8b99fb5a79412f7bcf346
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-11 10:52:07 +0300

    security config

--- src/main/java/com/etiya/fullstack/controllers/AuthController.java
+++ src/main/java/com/etiya/fullstack/controllers/AuthController.java
@@ -1,9 +1,14 @@
 package com.etiya.fullstack.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
+    @GetMapping
+    public String get(){
+        return "Merhaba";
+    }
 }
--- src/main/java/com/etiya/fullstack/core/configurations/SecurityConfiguration.java
+++ src/main/java/com/etiya/fullstack/core/configurations/SecurityConfiguration.java
@@ -1,14 +1,45 @@
 package com.etiya.fullstack.core.configurations;
 
+import com.etiya.fullstack.core.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private static final String[] WHITE_LIST_URLS = {
+            "api/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+    };
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-            return null;
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.
+                    csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(req -> req
+                            .requestMatchers(WHITE_LIST_URLS).permitAll()
+                            .anyRequest().authenticated())
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authenticationProvider(authenticationProvider)
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+            return http.build();
     }
 }
